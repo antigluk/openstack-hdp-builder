0c0779ac6b8029129211d511c901f335a00a9c92
Roman Rader <antigluk@gmail.com>
2013-07-15 19:51:13 +0300

    cloud-init installing on provisioning

--- provisioners/hdp-provision.sh
+++ provisioners/hdp-provision.sh
@@ -7,3 +7,10 @@ yum -y install ambari-server ambari-agent
 
 # ====== HDP =======
 wget -nv http://public-repo-1.hortonworks.com/HDP-1.2.0/repos/centos6/hdp.repo -O /etc/yum.repos.d/hdp.repo
+
+# ====== EPEL =======
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -Uvh epel-release-6*.rpm
+
+# ====== cloud tools ======
+yum install -y cloud-init
