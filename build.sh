0a14c4b9755f2e12f1bd1110bdbbbabd425ee7e9
Roman Rader <antigluk@gmail.com>
2013-07-15 18:53:04 +0300

    proper way to change timeout and splash

--- build.sh
+++ build.sh
@@ -3,6 +3,16 @@
 set -e
 set -x
 
+if ! which qemu-img; then
+	echo "No 'qemu-img' application"
+	exit 1
+fi
+
+if ! which packer; then
+	echo "No 'packer' application"
+	exit 1
+fi
+
 packer build packer-virtualbox-*hdp.json
 rm -f output-*/hdp-centos-6.4-64.qcow2
 qemu-img convert -O qcow2 output-virtualbox/packer-disk1.vmdk output-virtualbox/hdp-centos-6.4-64.qcow2
