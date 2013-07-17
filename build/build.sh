28c954a468e763a547bc03891841154a3e7882d5
Roman Rader <antigluk@gmail.com>
2013-07-17 17:39:46 +0300

    Template engine

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Builds virtual machine for OpenStack.
+# You can pass configuration file as first argument. Default config is 'default'
+# Configuration files are looked at 'templates/configname.conf'
 
 set -e
 set -x
@@ -13,6 +16,33 @@ if ! which packer; then
 	exit 1
 fi
 
+
+# Function makes file from template
+# - $1 - first parameter is template file. Base directory is 'templates/' folder
+# - $2 - second parameter is target location
+function process_template() {
+	mkdir -p "`dirname $2`"
+	cp "templates/$1" "$2"
+
+	# Replace all %{VARIABLE} to enviromnent variable
+	perl -p -i -e 's/\%\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' "$2"
+}
+
+# ==== MAKING CONFIGURATION FILES ====
+
+# Template variables set file
+[ -z "$1" ] && TEMPLATE="default" || TEMPLATE="$1"
+
+(
+	source "templates/$TEMPLATE.conf";
+	process_template anaconda-ks.cfg http_directory/anaconda-ks.cfg
+	process_template hdp-provision.sh provisioners/hdp-provision.sh
+)
+
+# ==== DO BUILDING ====
+
 packer build packer-virtualbox-*hdp.json
 rm -f output-*/hdp-centos-6.4-64.qcow2
+
+# ==== CONVERT TO QCOW2 FORMAT ====
 qemu-img convert -O qcow2 output-virtualbox/packer-disk1.vmdk output-virtualbox/hdp-centos-6.4-64.qcow2
--- build/templates/hdp-provision.sh
+++ build/templates/hdp-provision.sh
@@ -2,11 +2,11 @@
 
 # ====== Ambari =======
 cd /tmp
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/1.x/GA/ambari.repo -O /etc/yum.repos.d/ambari.repo
+wget -nv %{AMBARI_REPO} -O /etc/yum.repos.d/ambari.repo
 yum -y install ambari-server ambari-agent
 
 # ====== HDP =======
-wget -nv http://public-repo-1.hortonworks.com/HDP-1.2.0/repos/centos6/hdp.repo -O /etc/yum.repos.d/hdp.repo
+wget -nv %{HDP_REPO} -O /etc/yum.repos.d/hdp.repo
 
 # ====== EPEL =======
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
